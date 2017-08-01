514d51605afc98e42c56a1717926174efa04a781
Lifetime520 <lifetime520@gmail.com>
2017-08-01 17:26:55 +0800

    Title:Exclusive Time Of Functions URL:https://leetcode.com/problems/exclusive-time-of-functions/description/

---
+++ src/org/cast/leetcode/articles/ExclusiveTimeOfFunctions.java
@@ -0,0 +1,49 @@
+package org.cast.leetcode.articles;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ExclusiveTimeOfFunctions {
+	public static String[] pattern1 = {"0:start:0", "1:start:2", "1:end:5", "0:end:6"};
+	public static String[] pattern2 = {"0:start:0", "1:start:2", "1:end:5", "2:start:6", "2:end:7", "0:end:9"};
+	public static String[] pattern3 = {"0:start:0", "0:start:2", "0:end:5", "0:start:6", "0:end:6", "0:end:7"};
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<>();
+		Arrays.stream(pattern1).forEach(str -> list.add(str));
+		System.out.println(Arrays.toString(new Solution().exclusiveTime(2, list))); // [3, 4]
+
+		Arrays.stream(pattern2).forEach(str -> list.add(str));
+		System.out.println(Arrays.toString(new Solution().exclusiveTime(3, list))); // [4, 4, 2]
+
+		Arrays.stream(pattern3).forEach(str -> list.add(str));
+		System.out.println(Arrays.toString(new Solution().exclusiveTime(1, list))); // [8]
+	}
+}
+
+class Solution {
+	public int[] exclusiveTime(int n, List<String> logs) {
+		return calc(new int[n], logs, new ArrayList<>(), 0, 0, false);
+	}
+	public int[] calc(int[] ans, List<String> logs, List<Integer> stackStatus, int lastIdx, int lastTime, boolean isLastEnd) {
+		if (logs.isEmpty()) return ans;
+		String[] str = logs.remove(0).split(":");
+		int s = Integer.valueOf(str[0]);
+		int e = Integer.valueOf(str[2]);
+		int diff = e - lastTime;
+		if (str[1].equals("start")) {
+			ans[isLastEnd ? s : lastIdx] += ((isLastEnd) ? 0 : diff);
+			if (isLastEnd && !stackStatus.isEmpty())
+				ans[stackStatus.get(stackStatus.size() - 1)] += diff - 1;
+			else if (!isLastEnd && e != 0)
+				stackStatus.add(lastIdx);
+			return calc(ans, logs, stackStatus, s, e, false);
+		} else {
+			ans[s] += diff + (isLastEnd ? 0 : 1);
+			if (isLastEnd)
+				stackStatus.remove(stackStatus.size() - 1);
+			return calc(ans, logs, stackStatus, s, e, true);
+		}
+	}
+}
