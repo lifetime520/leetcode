ed8f7751753e2ea54d537f889018435f4e0a9902
Lifetime520 <lifetime520@gmail.com>
2017-08-08 18:03:51 +0800

    新增 交錯字串 解題

--- src/org/cast/leetcode/problems/interleavingString/InterleavingString.java
+++ src/org/cast/leetcode/problems/interleavingString/InterleavingString.java
@@ -2,11 +2,33 @@ package org.cast.leetcode.problems.interleavingString;
 
 public class InterleavingString {
 	public static String[] PATTERN_1 = {"aabcc", "dbbca", "aadbbcbcac"};
-	public static String[] PATTERN_2 = {"aabcc", "dbbca", "aadbbbaccc"};
+	public static String[] PATTERN_2 = {"", "", ""};
+	public static String[] PATTERN_3 = {"a", "", "a"};
+	public static String[] PATTERN_4 = {"", "", "a"};
+	public static String[] PATTERN_5 = {"a", "a", ""};
+	public static String[] PATTERN_6 = {
+			"bbbbbabbbbabaababaaaabbababbaaabbabbaaabaaaaababbbababbbbbabbbbababbabaabababbbaabababababbbaaababaa",
+			"babaaaabbababbbabbbbaabaabbaabbbbaabaaabaababaaaabaaabbaaabaaaabaabaabbbbbbbbbbbabaaabbababbabbabaab",
+			"babbbabbbaaabbababbbbababaabbabaabaaabbbbabbbaaabbbaaaaabbbbaabbaaabababbaaaaaabababbababaababbababbbababbbbaaaabaabbabbaaaaabbabbaaaabbbaabaaabaababaababbaaabbbbbabbbbaabbabaabbbbabaaabbababbabbabbab"
+	};
+	public static String[] PATTERN_7 = {"a", "", "c"};
+	public static String[] PATTERN_8 = {"db", "b", "cbb"};
 
 	public static void main(String[] args) {
 		System.out.println(new Solution().isInterleave(PATTERN_1[0], PATTERN_1[1], PATTERN_1[2])); // true
 
-		System.out.println(new Solution().isInterleave(PATTERN_2[0], PATTERN_2[1], PATTERN_2[2])); // false
+		System.out.println(new Solution().isInterleave(PATTERN_2[0], PATTERN_2[1], PATTERN_2[2])); // true
+
+		System.out.println(new Solution().isInterleave(PATTERN_3[0], PATTERN_3[1], PATTERN_3[2])); // true
+
+		System.out.println(new Solution().isInterleave(PATTERN_4[0], PATTERN_4[1], PATTERN_4[2])); // false
+
+		System.out.println(new Solution().isInterleave(PATTERN_5[0], PATTERN_5[1], PATTERN_5[2])); // false
+
+		System.out.println(new Solution().isInterleave(PATTERN_6[0], PATTERN_6[1], PATTERN_6[2])); // false
+
+		System.out.println(new Solution().isInterleave(PATTERN_7[0], PATTERN_7[1], PATTERN_7[2])); // false
+
+		System.out.println(new Solution().isInterleave(PATTERN_8[0], PATTERN_8[1], PATTERN_8[2])); // false
 	}
 }
--- src/org/cast/leetcode/problems/interleavingString/Solution.java
+++ src/org/cast/leetcode/problems/interleavingString/Solution.java
@@ -2,6 +2,80 @@ package org.cast.leetcode.problems.interleavingString;
 
 public class Solution {
 	public boolean isInterleave(String s1, String s2, String s3) {
+		int[][] memo = new int[s1.length()][s2.length()];
+		for (int i = 0; i < s1.length(); i++) {
+			for (int j = 0; j < s2.length(); j++) {
+				memo[i][j] = -1;
+			}
+		}
+		return isInterleave(s1.toCharArray(), s2.toCharArray(), s3.toCharArray(), 0, 0, 0, memo);
+	}
+
+	public boolean isInterleave(char[] s1, char[] s2, char[] s3, int i1, int i2, int i3, int[][] memo) {
+		if (s1.length == i1) {
+			return equals(s2, i2, s3, i3);
+		}
+		if (s2.length == i2) {
+			return equals(s1, i1, s3, i3);
+		}
+		if (s3.length == i3) {
+			return s1.length == i1 && s2.length == i2;
+		}
+		if (memo[i1][i2] >= 0) {
+			return memo[i1][i2] == 1;
+		}
+
+		boolean caseS1 = s1[i1] == s3[i3];
+		boolean caseS2 = s2[i2] == s3[i3];
+		boolean ans = false;
+		if (caseS1 && caseS2) {
+			ans = isInterleave(s1, s2, s3, i1 + 1, i2, i3 + 1, memo) || isInterleave(s1, s2, s3, i1, i2 + 1, i3 + 1, memo);
+		} else if (caseS1) {
+			ans = isInterleave(s1, s2, s3, i1 + 1, i2, i3 + 1, memo);
+		} else if (caseS2) {
+			ans = isInterleave(s1, s2, s3, i1, i2 + 1, i3 + 1, memo);
+		} else {
+			ans = false;
+		}
+		memo[i1][i2] = ans ? 1 : 0;
+		return ans;
+	}
+
+	public boolean equals(char[] c1, int offer1, char[] c2, int offer2) {
+		if (c1.length - offer1 != c2.length - offer2) return false;
+		while (c2.length > offer2 && c1.length > offer1) {
+			if (c1[offer1++] != c2[offer2++]) return false;
+		}
 		return true;
+	}
+}
+
+class SolutionX {
+	public boolean is_Interleave(String s1, int i, String s2, int j, String s3, int k, int[][] memo) {
+		if (i == s1.length()) {
+			return s2.substring(j).equals(s3.substring(k));
+		}
+		if (j == s2.length()) {
+			return s1.substring(i).equals(s3.substring(k));
+		}
+		if (memo[i][j] >= 0) {
+			return memo[i][j] == 1 ? true : false;
+		}
+		boolean ans = false;
+		if (s3.charAt(k) == s1.charAt(i) && is_Interleave(s1, i + 1, s2, j, s3, k + 1, memo)
+				|| s3.charAt(k) == s2.charAt(j) && is_Interleave(s1, i, s2, j + 1, s3, k + 1, memo)) {
+			ans = true;
+		}
+		memo[i][j] = ans ? 1 : 0;
+		return ans;
+	}
+	public boolean isInterleave(String s1, String s2, String s3) {
+		int memo[][] = new int[s1.length()][s2.length()];
+		for (int i = 0; i < s1.length(); i++) {
+			for (int j = 0; j < s2.length(); j++) {
+				memo[i][j] = -1;
+			}
+		}
+		return is_Interleave(s1, 0, s2, 0, s3, 0, memo);
 	}
 }
