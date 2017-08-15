5131da35314836b16d77f0dd3506fc7cdd6ae40d
Lifetime520 <lifetime520@gmail.com>
2017-08-16 02:39:48 +0800

    fixed some commit not yet

--- src/org/cast/leetcode/problems/reverseString/Solution.java
+++ src/org/cast/leetcode/problems/reverseString/Solution.java
@@ -2,6 +2,11 @@ package org.cast.leetcode.problems.reverseString;
 
 public class Solution {
 	public String reverseString(String s) {
-		return "";
+		char[] cs = new char[s.length()];
+		int i = s.length() - 1;
+		for (char c : s.toCharArray()) {
+			cs[i--] = (char) c;
+		}
+		return new String(cs);
 	}
 }
