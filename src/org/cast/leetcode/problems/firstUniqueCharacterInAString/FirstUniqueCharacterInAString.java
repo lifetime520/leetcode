b1554fcac7df9fd3f787b8cd7b4b77663fb6ceda
Lifetime520 <lifetime520@gmail.com>
2017-08-18 21:40:51 +0800

    add FirstUniqueCharacterInAString solution

--- src/org/cast/leetcode/problems/firstUniqueCharacterInAString/FirstUniqueCharacterInAString.java
+++ src/org/cast/leetcode/problems/firstUniqueCharacterInAString/FirstUniqueCharacterInAString.java
@@ -3,10 +3,16 @@ package org.cast.leetcode.problems.firstUniqueCharacterInAString;
 public class FirstUniqueCharacterInAString {
 	public static String PATTERN_1 = "leetcode";
 	public static String PATTERN_2 = "loveleetcode";
+	public static String PATTERN_3 = "";
+	public static String PATTERN_4 = "cc";
 
 	public static void main(String[] args) {
 		System.out.println(new Solution().firstUniqChar(PATTERN_1)); // 0
 
 		System.out.println(new Solution().firstUniqChar(PATTERN_2)); // 2
+
+		System.out.println(new Solution().firstUniqChar(PATTERN_3)); // -1
+
+		System.out.println(new Solution().firstUniqChar(PATTERN_4)); // -1
 	}
 }
--- src/org/cast/leetcode/problems/firstUniqueCharacterInAString/Solution.java
+++ src/org/cast/leetcode/problems/firstUniqueCharacterInAString/Solution.java
@@ -2,6 +2,21 @@ package org.cast.leetcode.problems.firstUniqueCharacterInAString;
 
 public class Solution {
 	public int firstUniqChar(String s) {
-		return 0;
+		if (s.length() == 0) return -1;
+
+		char[] cs = s.toCharArray();
+		int[] counting = new int[26];
+		int[] indexing = new int[26];
+		for (int i = 0; i < cs.length; i++) {
+			final int idx = cs[i] - 'a';
+			counting[idx]++;
+			indexing[idx] = i;
+		}
+		for (int i = 0; i < cs.length; i++) {
+			final int idx = cs[i] - 'a';
+			if (counting[idx] == 1)
+				return indexing[idx];
+		}
+		return -1;
 	}
 }
